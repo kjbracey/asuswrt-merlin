fcb91bc7ef48f21ee34fb12965ce360b3ff7366b
John Bacho <john.9527@hotmail.com>
2021-02-24 09:00:18 -0700

    others: just log result of renew-acme.sh without pipe to syslog

--- release/src/router/others/renew-acme.sh
+++ release/src/router/others/renew-acme.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Auto update of LetsEncrypt certs
-/jffs/acme.sh/acme.sh --cron --home /jffs/acme.sh | tee /tmp/acme.log | logger -t "ACME"
+/jffs/acme.sh/acme.sh --cron --home /jffs/acme.sh >/tmp/acme.log
 
 # Add additional cert post-processing here
 
