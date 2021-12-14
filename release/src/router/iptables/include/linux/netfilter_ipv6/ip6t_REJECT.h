0b382ea25791e2bb64a007a0587e759d7542e2f8
Kevin Bracey <kevin@bracey.fi>
2021-12-14 17:17:32 +0200

    Update ip6t_REJECT in kernel 2.6.22
    And adjust iptables (1.3.8) to match. This gives user-space
    compatibility between kernel 2.6.22 and 2.6.38.

--- release/src/router/iptables/include/linux/netfilter_ipv6/ip6t_REJECT.h
+++ release/src/router/iptables/include/linux/netfilter_ipv6/ip6t_REJECT.h
@@ -1,18 +1,22 @@
 #ifndef _IP6T_REJECT_H
 #define _IP6T_REJECT_H
 
+#include <linux/types.h>
+
 enum ip6t_reject_with {
 	IP6T_ICMP6_NO_ROUTE,
 	IP6T_ICMP6_ADM_PROHIBITED,
 	IP6T_ICMP6_NOT_NEIGHBOUR,
 	IP6T_ICMP6_ADDR_UNREACH,
 	IP6T_ICMP6_PORT_UNREACH,
-	IP6T_ICMP6_SRC_ADDR_FAIL_POLICY,
-	IP6T_TCP_RESET
+	IP6T_ICMP6_ECHOREPLY,
+	IP6T_TCP_RESET,
+	IP6T_ICMP6_POLICY_FAIL,
+	IP6T_ICMP6_REJECT_ROUTE
 };
 
 struct ip6t_reject_info {
-	enum ip6t_reject_with with;      /* reject type */
+	__u32	with;	/* reject type */
 };
 
 #endif /*_IP6T_REJECT_H*/
